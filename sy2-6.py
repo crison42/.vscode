af1ff3d7704048bcc94ac9124232ca8920e693aa
Noob <changhong356@gmail.com>
2023-10-24 14:10:41 +0800

    modified:   sy2-6.py

--- sy2-6.py
+++ sy2-6.py
@@ -1,21 +1,27 @@
 score = {'001': 96, '002': 98, '003': 92, '004': 93, '005': 94}
 print(score)
+
 score['006'] = 100
 print(score)
+
 score['002'] = 99
 print(score)
+
 del (score['001'])
 print(score)
+
 print(score['004'])
 index=0
 for i in score:
     if score[i]>index:
         index=score[i]
 print(index)
+
 for j in score:
         if score[j]<index:
             index=score[j]
 print(index)
+
 sum=0
 for k in score:
         sum+=int(score[k])
