412c05a9669c83a3eeff53aa758496c95200d9a9
Noob <changhong356@gmail.com>
2023-10-26 17:41:41 +0800

    modified:   sy3-2.py

--- sy3-2.py
+++ sy3-2.py
@@ -2,8 +2,14 @@ def username_test(username,list):
     for letter in range(0,list.__len__()):
         if list[letter] == username:   
             return letter
-        else:
-            return False
+    return -1
+
+def password_test(password,list,letter):
+    if list[letter] == password:
+        return True
+    else:
+        return False
+# 该函数可不使用，但需要mian函数内注释行
 
 def main():
     userName = ("zhangsan","lisi","wangwu","zhaoliu","xiaolin","xiaohua")
@@ -12,7 +18,7 @@ def main():
     while True:
         username = input("请输入用户名:")
         letter=username_test(username,userName)
-        if letter==False:
+        if letter==-1:
             print("用户不存在")
             flag += 1
             if flag == 3:
@@ -24,13 +30,15 @@ def main():
     flag=0
     while True:
         password = input("请输入密码:")
-        if pwList[letter] == password:
-            print("密码错误")
+        # if pwList[letter] != password:
+        if password_test(password,pwList,letter)==False:
+            print("密码不正确")
             flag += 1
             if flag == 3:
                 print("密码输入超过3次")
                 break
         else:
             print("登录成功")
+            break
 
 main()
