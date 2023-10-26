594be7ae41669e8e7bc0e9200a7aa9795515a0d3
Noob <changhong356@gmail.com>
2023-10-26 17:29:35 +0800

    modified:   sy3-1.py 	sy3-2.py

--- sy3-1.py
+++ sy3-1.py
@@ -1,8 +1,8 @@
 def is_palindrome(s: str) -> bool:
    return s == s[::-1]
 
-# Test cases
-print(is_palindrome("121"))  # True
-print(is_palindrome("123454321"))  # True
-print(is_palindrome("abba"))  # True
-print(is_palindrome("hello"))  # False
+string = input("请输入字符串")
+if is_palindrome(string):
+    print(string+" 是回文")
+else:
+    print(string+" 不是回文")
