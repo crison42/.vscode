c1ca76987001741ceb399d95410266d25a1061f6
Noob <changhong356@gmail.com>
2023-10-26 18:13:00 +0800

    new file:   sy3-7.py 	modified:   test.py

--- test.py
+++ test.py
@@ -1,6 +1,25 @@
-sum =0
-for i in range(100):
-    if (i%10):
-        continue
-    sum=sum+i
-print(sum)
+def deco(func):
+    def wrapper():
+        print(f"before {func.__name__} called.")
+        func()
+        print(f"after {func.__name__} called.")
+    return wrapper
+
+@deco
+def myfunc1():
+    print("myfunc1() called.")
+
+@deco
+def myfunc2():
+    print("myfunc2() called")
+
+myfunc1()
+myfunc2()
+
+# Output:
+# before myfunc1 called.
+# myfunc1() called.
+# after myfunc1 called.
+# before myfunc2 called.
+# myfunc2() called
+# after myfunc2 called.
