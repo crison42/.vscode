4db2969f56b10a4ed631d20b50c6a288843a762f
Noob <changhong356@gmail.com>
2023-11-28 21:44:59 +0800

    modified:   sy5-3.py

--- sy5-3.py
+++ sy5-3.py
@@ -1,7 +1,7 @@
 from lxml import etree
 import requests
 import time  # 这里导入时间模块，以免豆瓣封你IP
-import os
+# import os
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36"  # 伪装成浏览器
