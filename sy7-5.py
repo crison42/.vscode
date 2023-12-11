cd1beaa336239bc313f1957cbe2fabb658564fcf
Noob <changhong356@gmail.com>
2023-12-11 17:33:13 +0800

    modified:   sy7-5.py

--- sy7-5.py
+++ sy7-5.py
@@ -6,17 +6,10 @@ while True:
         # 尝试将输入的字符串转换为整数
         num = int(data)
 
-        # 如果转换失败，则捕获TypeError异常
-        if not isinstance(num, int):
-            raise ValueError("输入的数据不是整数")
-
         if num == 0:
-            
             raise ZeroDivisionError("0不能作除数")
 
         if num == 1:
-            alist = [1,2,3]
-            print(alist[4])
             raise IndexError("下标越界了")
 
         if num == 2:
@@ -31,6 +24,9 @@ while True:
         if num == 5:
             raise AttributeError("欢迎来到没有异常的分支")
 
+    except ValueError:
+        print("输入的不是0-4,没有发生异常")
+        break
     except Exception as e:
         print(e)
     else:
