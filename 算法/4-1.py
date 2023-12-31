21e590dd8b8bc1ba88d902491c329b823542b4ea
Noob <changhong356@gmail.com>
2023-12-31 18:10:36 +0800

    modified:   .vscode/settings.json 	modified:   "\347\256\227\346\263\225/4-1.py" 	modified:   "\347\256\227\346\263\225/4-2.py"

--- 算法/4-1.py
+++ 算法/4-1.py
@@ -1,29 +1,33 @@
-# 定义一个函数knapsack，接收两个参数：物品列表items和背包的最大承重max_weight
+# 定义一个函数 knapsack，用于解决背包问题
+# 参数 items 是一个物品列表，每个物品是一个元组，包含重量和价值
+# 参数 max_weight 是背包的最大承重
 def knapsack(items, max_weight):
-   # 定义一个内部函数backtrack，接收三个参数：当前物品索引i、当前背包的重量cw和当前背包的价值cv
-   def backtrack(i, cw, cv):
-       # 如果当前背包的重量超过最大承重，或者已经遍历完所有物品，则返回
-       if cw > max_weight or i == len(items):
-           # 如果当前背包的价值大于已记录的最大价值，则更新最大价值
-           if cv > max_value:
-               max_value = cv
-           return
-       # 不选当前物品，继续递归遍历下一个物品
-       backtrack(i + 1, cw, cv)
-       # 如果当前背包的重量加上当前物品的重量不超过最大承重，则选当前物品，继续递归遍历下一个物品
-       if cw + items[i][0] <= max_weight:
-           backtrack(i + 1, cw + items[i][0], cv + items[i][1])
+    # 初始化最大价值为0
+    max_value = [0]
 
-   # 初始化最大价值为0
-   max_value = 0
-   # 开始递归遍历
-   backtrack(0, 0, 0)
-   # 返回最大价值
-   return max_value
+    # 定义一个回溯函数 backtrack
+    def backtrack(i, cw, cv):
+        # 如果当前物品的重量超过背包的最大承重，或者已经遍历完所有物品，返回
+        if cw > max_weight or i == len(items):
+            if cv > max_value[0]:
+                max_value[0] = cv
+            return
+        # 不选当前物品，继续遍历下一个物品
+        backtrack(i + 1, cw, cv)
+        # 如果当前物品的重量加上背包的剩余承重小于等于背包的最大承重，选择当前物品，继续遍历下一个物品
+        if cw + items[i][0] <= max_weight:
+            backtrack(i + 1, cw + items[i][0], cv + items[i][1])
 
-# 定义物品列表items，格式为(重量, 价值)
+    # 调用回溯函数，从第一个物品开始遍历
+    backtrack(0, 0, 0)
+
+    # 返回最大价值
+    return max_value[0]
+
+
+# 定义物品列表
 items = [(2, 3), (3, 4), (4, 5), (5, 8)]
-# 定义背包的最大承重max_weight
+# 定义背包的最大承重
 max_weight = 10
-# 调用knapsack函数，并打印最大价值
-print("Maximum value in knapsack:", knapsack(items, max_weight))
+# 调用 knapsack 函数，计算最大价值
+print("最大价值为:", knapsack(items, max_weight))
