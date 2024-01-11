ecee8a2be7502ff180f72ee6192596c373c9ab9d
Noob <changhong356@gmail.com>
2024-01-11 14:00:54 +0800

    new file:   kcsj/__pycache__/6-1.cpython-311.pyc 	modified:   kcsj/test.py

--- kcsj/test.py
+++ kcsj/test.py
@@ -1,6 +1,24 @@
-import e6-1
-import 6-2
-import 6-3
-import 6-4
-import 6-5
-import 6-6
+str=input("请输入想要使用的算法："+"\n"
+          +"1.冒泡排序"+"\n"
+    +"2.选择排序"+"\n"
+    +"3.插入排序"+"\n"
+    +"4.快速排序"+"\n"
+    +"5.归并排序"+"\n"
+    +"6.希尔排序"+"\n")
+imported_module = ""
+if str=="1":
+    imported_module=__import__("6-1")
+elif str=="2":
+    imported_module=__import__("6-2")
+elif str=="3":
+    imported_module=__import__("6-3")
+elif str=="4":
+    imported_module=__import__("6-4")
+elif str=="5":
+    imported_module=__import__("6-5")
+elif str=="6":
+    imported_module=__import__("6-6")
+else:
+    print("输入错误")
+imported_module.main()
+
