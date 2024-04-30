20cac51fa76047e291346ce59dbb5b1b55d158d3
noob <changhong356@gmail.com>
2024-04-30 16:57:00 +0800

    new file:   html/sy8/code1/.gitignore 	new file:   html/sy8/code1/.vscode/extensions.json 	new file:   html/sy8/code1/README.md 	new file:   html/sy8/code1/index.html 	new file:   html/sy8/code1/package-lock.json 	new file:   html/sy8/code1/package.json 	new file:   html/sy8/code1/public/vite.svg 	new file:   html/sy8/code1/src/App.vue 	new file:   html/sy8/code1/src/assets/vue.svg 	new file:   html/sy8/code1/src/components/MyShop.vue 	new file:   html/sy8/code1/src/main.js 	new file:   html/sy8/code1/src/style.css 	new file:   html/sy8/code1/vite.config.js 	new file:   html/sy8/code2/.gitignore 	new file:   html/sy8/code2/.vscode/extensions.json 	new file:   html/sy8/code2/README.md 	new file:   html/sy8/code2/index.html 	new file:   html/sy8/code2/package-lock.json 	new file:   html/sy8/code2/package.json 	new file:   html/sy8/code2/public/vite.svg 	new file:   html/sy8/code2/src/App.vue 	new file:   html/sy8/code2/src/assets/bg.png 	new file:   html/sy8/code2/src/assets/film1.png 	new file:   html/sy8/code2/src/assets/vue.svg 	new file:   html/sy8/code2/src/components/MyFilm.vue 	new file:   html/sy8/code2/src/main.js 	new file:   html/sy8/code2/src/style.css 	new file:   html/sy8/code2/vite.config.js

---
+++ html/sy8/code1/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <my-shop></my-shop>       <!-- 引用组件 -->
+</template>
+
+<script setup>
+import MyShop from './components/MyShop.vue'   //导入并局部注册组件MyShop.vue
+</script> 
---
+++ html/sy8/code2/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <MyFilm></MyFilm>       <!-- 引用组件 -->
+</template>
+
+<script setup>
+import MyFilm from './components/MyFilm.vue'   //导入并局部注册组件MyFilm.vue
+</script>
